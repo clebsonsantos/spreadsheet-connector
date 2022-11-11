a08a3737cd1619a1d416a7c8215c25e0c99a93db
Clebson Santos <clebsonsantosmtt@gmail.com>
2022-11-11 11:51:00 -0300

    use entity to validate values to datasheet

--- src/usecases/add-values-datasheet.ts
+++ src/usecases/add-values-datasheet.ts
@@ -1,9 +1,16 @@
 import { AddValues } from "../contracts/usecases"
+import { DataSheet } from "../entities"
 import { left, right } from "../main/shared"
 
 export class AddValuesDataSheet implements AddValues {
   async execute ({ auth, data }: AddValues.Params): Promise<AddValues.Result> {
-    const values = await auth.addValues({ values: data })
+    const datasheet = new DataSheet({ data })
+
+    if (!datasheet.isValid()) {
+      return left(new Error("Invalid values"))
+    }
+
+    const values = await auth.addValues({ values: datasheet.data })
     return values.isLeft()
       ? left(values.value)
       : right(values.value)
